7533233b7a988b8750d94a904ed7ad3c99abe28c
maktheus <maktheus@gmail.com>
2024-02-04 00:59:11 -0400

    singal Extensor done ✅

--- Modules/signalExtensor/signalExtensor.v
+++ Modules/signalExtensor/signalExtensor.v
@@ -1,7 +1,9 @@
-module signext(input logic [15:0] a,
-output logic [31:0] y);
+module siganalNext(
+    input  [`SIGN_EXTEND_LEN-1:0] inputSignalBeforeExtension,
+    output  [`WORD_LEN-1:0] outputSignalAfterExtension);
 
-    
-    assign y = {{16{a[15]}}, a};
+    // Estender o sinal do bit mais significativo (bit 15) da entrada de 16 bits
+    // e concatenar com a entrada original para criar uma saída de 32 bits.
+    assign outputSignalAfterExtension = {{`SIGN_EXTEND_LEN{inputSignalBeforeExtension[`SIGN_EXTEND_LEN-1]}}, inputSignalBeforeExtension};
 
 endmodule
