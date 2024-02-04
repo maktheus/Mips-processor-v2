29ed4e70ff481108112f16927f06a4dc2e8f4b82
maktheus <maktheus@gmail.com>
2024-02-04 16:30:06 -0400

    update

---
+++ Modules/signalExtensor/signalExtensorTb.v
@@ -0,0 +1,32 @@
+`include "../../config/config.v"
+
+`timescale 1ns / 1ps
+module siganalNext_tb;
+
+// Sinais do Testbench
+reg [`SIGN_EXTEND_LEN-1:0] inputSignalBeforeExtension;
+wire [`WORD_LEN-1:0] outputSignalAfterExtension;
+
+// Instanciação do módulo a ser testado
+siganalNext uut (
+    .inputSignalBeforeExtension(inputSignalBeforeExtension),
+    .outputSignalAfterExtension(outputSignalAfterExtension)
+);
+
+initial begin
+    // Inicialização
+    inputSignalBeforeExtension = 0;
+
+    // Aplicação de estímulos
+    #10 inputSignalBeforeExtension = 16'h8000; // Teste com bit MSB = 1
+    #10 inputSignalBeforeExtension = 16'h7FFF; // Teste com bit MSB = 0
+
+    #20 $finish; // Finaliza a simulação
+end
+
+initial begin
+    $monitor("At time %t, input = %h, output = %h",
+             $time, inputSignalBeforeExtension, outputSignalAfterExtension);
+end
+
+endmodule
--- config/config.v
+++ config/config.v
@@ -2,3 +2,6 @@
 `define EXE_CMD_LEN 4
 `define RA_LEN 4
 `define RAM_DEPTH 64
+`define MEMORY_ADDR_LEN 7
+`define SIGN_EXTEND_LEN 16
+`define PC_LEN 32
