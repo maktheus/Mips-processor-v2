a048677a3f92f8dbcc1975b9473ee2cd77e89294
maktheus <maktheus@gmail.com>
2024-02-09 23:27:21 -0400

    first

--- Modules/ProgamingCounting/pcModule.v
+++ Modules/ProgamingCounting/pcModule.v
@@ -1,16 +1,18 @@
+`include "../../config/config.v"
+
 module ProgramCounter(
     input clk,            // Clock signal
     input reset,          // Asynchronous reset signal
     input enable,         // Enable signal to increment or load the PC
-    input [31:0] newPC,   // Input for loading a new PC value
-    output reg [31:0] PC  // Current PC value
+    input [`WORD_LEN-1:0] newPC,   // Input for loading a new PC value
+    output reg [`WORD_LEN-1:0] PC  // Current PC value
 );
 
 // On every positive edge of the clock or an active-high reset signal
 always @(posedge clk or posedge reset) begin
     if (reset) begin
         // Asynchronously reset the PC to 0
-        PC <= 32'b0;
+        PC <= `WORD_LEN'b0;
     end else if (enable) begin
         // If enabled, increment the PC by 4 (assuming 4-byte instruction width)
         // Alternatively, load a new value into the PC
